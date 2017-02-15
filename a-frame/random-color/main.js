7a5afbe27914f0bb328f3db10a428bec6caeb9cd
sebastian <none>
2017-02-15 17:32:45 +0100

    Add timing to the random color

--- a-frame/random-color/main.js
+++ a-frame/random-color/main.js
@@ -1,10 +1,19 @@
 AFRAME.registerComponent('random-color', {
     init: function () {
-        var el = this.el;
-        var mesh = el.getObject3D('mesh');
-        var color = mesh.material.color;
-        color.r = Math.random();
-        color.g = Math.random();
-        color.b = Math.random();
+        this.counter = 0;
+    },
+    tick: function (time, timeDelta) {
+        this.counter += timeDelta;
+
+        if (this.counter > 1000) {
+            this.counter = 0;
+
+            var el = this.el;
+            var mesh = el.getObject3D('mesh');
+            var color = mesh.material.color;
+            color.r = Math.random();
+            color.g = Math.random();
+            color.b = Math.random();
+        }
     }
 });

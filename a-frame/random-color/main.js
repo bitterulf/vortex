50d28d1b86e022baa7e6e01889ddcf2b44192720
sebastian <none>
2017-02-16 09:31:52 +0100

    Add interval property to control the color change

--- a-frame/random-color/main.js
+++ a-frame/random-color/main.js
@@ -1,11 +1,14 @@
 AFRAME.registerComponent('random-color', {
+    schema: {
+        interval: {type: 'int', default: 1000}
+    },
     init: function () {
         this.counter = 0;
     },
     tick: function (time, timeDelta) {
         this.counter += timeDelta;
 
-        if (this.counter > 1000) {
+        if (this.counter > this.data.interval) {
             this.counter = 0;
 
             var el = this.el;

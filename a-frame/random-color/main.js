d517f1ec643c483a592b36436fdf4e5bb562e86d
sebastian <none>
2017-02-16 15:22:15 +0100

    Add interpolation

--- a-frame/random-color/main.js
+++ a-frame/random-color/main.js
@@ -10,17 +10,30 @@ AFRAME.registerComponent('random-color', {
                 b: Math.random()
             }
         };
+
+        this.interpolateColor = function(color1, color2, value) {
+            return {
+                r: color1.r + (color2.r - color1.r) * value,
+                g: color1.g + (color2.g - color1.g) * value,
+                b: color1.b + (color2.b - color1.b) * value
+            }
+        };
+
         this.counter = 0;
+        this.startColor = this.randomColor();
+        this.destinationColor = this.randomColor();
     },
     tick: function (time, timeDelta) {
         this.counter += timeDelta;
 
         if (this.counter > this.data.interval) {
             this.counter = 0;
-
-            var el = this.el;
-            var mesh = el.getObject3D('mesh');
-            mesh.material.color = this.randomColor();
+            this.startColor = this.destinationColor;
+            this.destinationColor = this.randomColor();
         }
+
+        var el = this.el;
+        var mesh = el.getObject3D('mesh');
+        mesh.material.color = this.interpolateColor(this.startColor, this.destinationColor, this.counter / 1000);
     }
 });
